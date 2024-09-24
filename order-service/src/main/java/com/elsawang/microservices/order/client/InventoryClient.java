4dbd0e1313e4edef7c9ddb159e8223f20fcead3b
elsawang <elsa.yilin.w39@gmail.com>
2024-09-24 22:52:16 +0100

    change openFeign to spring HTTP Interface

--- order-service/src/main/java/com/elsawang/microservices/order/client/InventoryClient.java
+++ order-service/src/main/java/com/elsawang/microservices/order/client/InventoryClient.java
@@ -6,6 +6,8 @@ import org.springframework.web.service.annotation.GetExchange;
 
 
 public interface InventoryClient {
+
+    // HTTP interface
     @GetExchange("/api/inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 }
