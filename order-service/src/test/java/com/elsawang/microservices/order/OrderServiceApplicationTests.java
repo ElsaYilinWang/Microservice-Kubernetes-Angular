27c4da1a62d1b684943cb4c14a7209f8cef558ce
elsawang <elsa.yilin.w39@gmail.com>
2024-09-24 15:54:00 +0100

    add InventoryClientStub and update OrderServiceApplicationTests

--- order-service/src/test/java/com/elsawang/microservices/order/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/elsawang/microservices/order/OrderServiceApplicationTests.java
@@ -1,5 +1,6 @@
 package com.elsawang.microservices.order;
 
+import com.elsawang.microservices.order.stub.InventoryClientStub;
 import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,12 +8,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.testcontainers.containers.MySQLContainer;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
 //@Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
     @ServiceConnection
@@ -39,6 +42,9 @@ class OrderServiceApplicationTests {
                 	"quantity":1
                 	}
                 """;
+
+
+        InventoryClientStub.stubInventoryCall("iphone_15", 1);
         var responseBodyString = RestAssured.given()
                 .contentType("application/json")
                 .body(submitOrderJson)
