dd7d8677d8647c97a01d920f9a2ad1561b4245bf
elsawang <elsa.yilin.w39@gmail.com>
2024-09-24 11:37:18 +0100

    establish structures under inventory-service

---
+++ inventory-service/src/main/java/com/elsawang/microservices/inventory_service/controller/InventoryController.java
@@ -0,0 +1,19 @@
+package com.elsawang.microservices.inventory_service.controller;
+
+import com.elsawang.microservices.inventory_service.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/inventory")
+@RequiredArgsConstructor
+public class InventoryController {
+    private final InventoryService inventoryService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@RequestBody String skuCode, @RequestParam Integer quantity){
+        return inventoryService.isInStock(skuCode, quantity);
+    }
+}
