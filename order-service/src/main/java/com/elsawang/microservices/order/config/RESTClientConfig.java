f5315a4aee02dca42f981383bd6487f3b3e68773
elsawang <elsa.yilin.w39@gmail.com>
2024-09-25 11:32:02 +0100

    implement TimeOut through the requestFactory() in RESTClientConfig

--- order-service/src/main/java/com/elsawang/microservices/order/config/RESTClientConfig.java
+++ order-service/src/main/java/com/elsawang/microservices/order/config/RESTClientConfig.java
@@ -2,11 +2,16 @@ package com.elsawang.microservices.order.config;
 
 import com.elsawang.microservices.order.client.InventoryClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.time.Duration;
 
 @Configuration
 public class RESTClientConfig {
@@ -21,6 +26,14 @@ public class RESTClientConfig {
         var restClientAdapter = RestClientAdapter.create(restClient);
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
         return httpServiceProxyFactory.createClient(InventoryClient.class);
+    }
+
+    private ClientHttpRequestFactory getClientHttpRequestFactory() {
+        ClientHttpRequestFactorySettings clientHttpRequestFactorySettings = ClientHttpRequestFactorySettings.DEFAULTS
+                .withConnectTimeout(Duration.ofSeconds(3))
+                .withReadTimeout(Duration.ofSeconds(3));
+
+       return ClientHttpRequestFactories.get(clientHttpRequestFactorySettings);
     }
 }
 
