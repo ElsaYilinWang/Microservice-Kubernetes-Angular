64e9a0efb3194bb40ebf88b7edb4afa064cae0e5
elsawang <elsa.yilin.w39@gmail.com>
2024-09-25 17:18:33 +0100

    update OrderService with kafkaTemplate

--- order-service/src/main/java/com/elsawang/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/elsawang/microservices/order/service/OrderService.java
@@ -2,9 +2,12 @@ package com.elsawang.microservices.order.service;
 
 import com.elsawang.microservices.order.client.InventoryClient;
 import com.elsawang.microservices.order.dto.OrderRequest;
+import com.elsawang.microservices.order.event.OrderPlacedEvent;
 import com.elsawang.microservices.order.model.Order;
 import com.elsawang.microservices.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,17 +15,25 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 @Transactional
 public class OrderService {
 
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest) {
         boolean inStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
         if (inStock) {
             var order = mapToOrder(orderRequest);
             orderRepository.save(order);
+            // send the message to Kafka Topic
+            var orderPlacedEvent = new OrderPlacedEvent(order.getOrderNumber(), orderRequest.userDetails().email());
+
+            log.info("Start- Sending OrderPlacedEvent {} to Kafka Topic", orderPlacedEvent);
+            kafkaTemplate.send("order-placed", orderPlacedEvent);
+            log.info("End- Sending OrderPlacedEvent {} to Kafka Topic", orderPlacedEvent);
         } else {
             throw new RuntimeException("Product with Skucode " + orderRequest.skuCode() + "is not in stock");
         }
